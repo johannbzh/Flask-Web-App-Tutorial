19761ae5f1c78f8384466968d63aa874e8de96cf
johann bzaih <johann.bzaih@gmail.com>
2021-08-14 13:49:48 +0200

    fixed bug

--- website/__init__.py
+++ website/__init__.py
@@ -19,6 +19,7 @@ def create_app():
   app.register_blueprint(auth, url_prefix='/')
 
   from .models import User, Note
+
   create_database(app)
 
   login_manager = LoginManager()
--- website/views.py
+++ website/views.py
@@ -4,6 +4,6 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/') # homepage
-@login_required() # Can't access the homepage unless you're logged in
+@login_required # Can't access the homepage unless you're logged in
 def home():
   return render_template('home.html')
